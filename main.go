ea1e8ac4dc813605f0c78375137eb697bfe1c014
rayfranc <rayfranciscoplay@gmail.com>
2024-02-29 07:45:28 -0500

    Working on Exception Handler

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"main/config"
 	"main/controller"
 	"main/helper"
+	"main/middlewares"
 	model "main/models"
 	"main/repository"
 	"main/routes"
@@ -11,6 +12,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
 
@@ -20,17 +22,18 @@ func main(){
   db:=config.DatabaseConnection()
   validate:=validator.New()
   db.Table("people").AutoMigrate(&model.Person{})
-
+  app:=gin.Default()
+  app.Use(gin.CustomRecovery(middlewares.ErrorHandler))
 	personRepository:=repository.NewPersonRepositoryImpl(db)
 	personService:=services.NewPersonServiceImpl(personRepository,validate)
 	personController:=controller.NewPersonController(personService)
 
-    router := routes.PersonRoutes(personController)
+app = routes.PersonRoutes(personController,app)
 	
   
  server := &http.Server{
 	Addr:           ":8888",
-	Handler:        router,
+	Handler:        app,
 	ReadTimeout:    10 * time.Second,
 	WriteTimeout:   10 * time.Second,
 	MaxHeaderBytes: 1 << 20,
--- routes/person_routes.go
+++ routes/person_routes.go
@@ -2,14 +2,12 @@ package routes
 
 import (
 	"main/controller"
-	"main/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
-func PersonRoutes(personController *controller.PersonController) *gin.Engine{
-    router:=gin.Default()
-    router.Use(gin.CustomRecovery(middlewares.ErrorHandler))
+func PersonRoutes(personController *controller.PersonController, router *gin.Engine) *gin.Engine{
+    
     personRoutes:=router.Group("api/person")
     personRoutes.GET("",personController.FindAll)
     personRoutes.GET("/:personId",personController.FindById)
