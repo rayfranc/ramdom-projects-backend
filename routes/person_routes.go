86b6de662288d644708c71737e60e356a103bbcb
rayfranc <rayfranciscoplay@gmail.com>
2024-03-24 00:37:04 -0400

    feat: Setting up workflow

--- routes/person_routes.go
+++ routes/person_routes.go
@@ -8,12 +8,12 @@ import (
 
 func PersonRoutes(personController *controller.PersonController, router *gin.Engine) *gin.Engine{
     
-    personRoutes:=router.Group("api/person")
-    personRoutes.GET("",personController.FindAll)
-    personRoutes.GET("/:personId",personController.FindById)
-    personRoutes.POST("",personController.Create)
-    personRoutes.PUT("/:personId",personController.Update)
-    personRoutes.DELETE("/:personId",personController.Delete)
+    router.Group("api/person")
+    // personRoutes.GET("",personController.FindAll)
+    // personRoutes.GET("/:personId",personController.FindById)
+    // personRoutes.POST("",personController.Create)
+    // personRoutes.PUT("/:personId",personController.Update)
+    // personRoutes.DELETE("/:personId",personController.Delete)
 
     return router
 
